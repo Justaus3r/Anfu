5629eeb1fe0026a37fa4e457b04a09eff9982030
Justaus3r <72691864+Justaus3r@users.noreply.github.com>
2021-08-09 16:21:15 +0500

    Create:Create a bash notifier script

---
+++ _low_ram_notifier.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#©Justaus3r 2021
+#Distributed under GPLV3
+#  This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#    
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#This file is a part of Anfu,a Tui tool for encrypting files/folder
+
+VERSION=0.3.10
+
+echo ░█████╗░███╗░░██╗███████╗██╗░░░██╗
+echo ██╔══██╗████╗░██║██╔════╝██║░░░██║
+echo ███████║██╔██╗██║█████╗░░██║░░░██║
+echo ██╔══██║██║╚████║██╔══╝░░██║░░░██║
+echo ██║░░██║██║░╚███║██║░░░░░╚██████╔╝
+echo ╚═╝░░╚═╝╚═╝░░╚══╝╚═╝░░░░░░╚═════╝░$VERSION
+
+echo WARNING!
+echo Hey onichan!,you are using too much RAM,stop watching nudes and don\'t encrypt any file 
+echo greater than 100 Mb in meanwhile as it might trigger undesired interferance.
+
+read
